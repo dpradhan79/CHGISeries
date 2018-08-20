655bdb1d6e183f16a3b9e5b5499afba29a0e45b0
Debasish Pradhan <debasish.pradhan@gmail.com>
2018-08-20 06:21:59 -0600

    Added new class to add test cases for Vendor Search

---
+++ src/test/java/test/iseries/jdevendorsearch/TestVendorSearch.java
@@ -0,0 +1,36 @@
+package test.iseries.jdevendorsearch;
+
+import org.testng.annotations.Test;
+
+import dweb.test.templates.TestTemplate;
+import dweb.test.templates.TestTemplateMethodLevelInit;
+import pages.iseries.jdevendorsearch.JdeVendorSearchPage;
+import pages.iseries.loginpage.LoginPage;
+
+public class TestVendorSearch extends TestTemplateMethodLevelInit{
+
+	@Test
+	public void testC939958()
+	{
+		//Initialization
+		JdeVendorSearchPage jdeVendorSearchPage = new JdeVendorSearchPage(TestTemplate.threadLocalWebDriver.get(), TestTemplate.testReport);
+		
+		//Validating login info
+		jdeVendorSearchPage.validateInformationPresentOnLoginPage();
+
+	}
+	
+	@Test
+	public void testC939922()
+	{
+		//Initialization
+		LoginPage loginPage = new LoginPage(TestTemplate.threadLocalWebDriver.get(), TestTemplate.testReport);
+		JdeVendorSearchPage jdeVendorSearchPage = new JdeVendorSearchPage(TestTemplate.threadLocalWebDriver.get(), TestTemplate.testReport);
+		
+		//Logging in to application
+		loginPage.jdeVendorSearchLogin();
+		
+		//Validating is user logged in to application successfully.
+		jdeVendorSearchPage.validateLoggedInToApplication();
+	}
+}
