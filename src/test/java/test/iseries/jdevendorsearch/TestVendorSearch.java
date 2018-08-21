48b5db5956e2b5cb84cb74173e95df0d835ccd39
Anil <anil.talasila@cigniti.com>
2018-08-21 17:47:19 +0530

    Added 6 new test cases

--- src/test/java/test/iseries/jdevendorsearch/TestVendorSearch.java
+++ src/test/java/test/iseries/jdevendorsearch/TestVendorSearch.java
@@ -47,4 +47,74 @@ public class TestVendorSearch extends TestTemplateMethodLevelInit{
 		//Validate list of fields exist in search form
 		jdeVendorSearchPage.validateFieldsPresentInSearchForm();
 	}
+	
+	@Test
+	public void testC939924()
+	{
+		//Initialization
+		LoginPage loginPage = new LoginPage(TestTemplate.threadLocalWebDriver.get(), TestTemplate.testReport);
+		JdeVendorSearchPage jdeVendorSearchPage = new JdeVendorSearchPage(TestTemplate.threadLocalWebDriver.get(), TestTemplate.testReport);
+		
+		//Logging in to application
+		loginPage.jdeVendorSearchLogin();
+				
+		//Validate list of fields exist in search form
+		jdeVendorSearchPage.searchForAVendor("Verizon");
+	}
+	
+	@Test
+	public void testC939931()
+	{
+		//Initialization
+		LoginPage loginPage = new LoginPage(TestTemplate.threadLocalWebDriver.get(), TestTemplate.testReport);
+		JdeVendorSearchPage jdeVendorSearchPage = new JdeVendorSearchPage(TestTemplate.threadLocalWebDriver.get(), TestTemplate.testReport);
+		
+		//Logging in to application
+		loginPage.jdeVendorSearchLogin();
+				
+		//search with state and validate record
+		jdeVendorSearchPage.searchForAVendorWithState("TX");
+	}
+	
+	@Test
+	public void testC939932()
+	{
+		//Initialization
+		LoginPage loginPage = new LoginPage(TestTemplate.threadLocalWebDriver.get(), TestTemplate.testReport);
+		JdeVendorSearchPage jdeVendorSearchPage = new JdeVendorSearchPage(TestTemplate.threadLocalWebDriver.get(), TestTemplate.testReport);
+		
+		//Logging in to application
+		loginPage.jdeVendorSearchLogin();
+				
+		//search with zip and validate record
+		jdeVendorSearchPage.searchForAVendorWithZip("75266-0720");
+	}
+	
+	@Test
+	public void testC939933()
+	{
+		//Initialization
+		LoginPage loginPage = new LoginPage(TestTemplate.threadLocalWebDriver.get(), TestTemplate.testReport);
+		JdeVendorSearchPage jdeVendorSearchPage = new JdeVendorSearchPage(TestTemplate.threadLocalWebDriver.get(), TestTemplate.testReport);
+		
+		//Logging in to application
+		loginPage.jdeVendorSearchLogin();
+				
+		//search with Vendor Number and validate record
+		jdeVendorSearchPage.searchForAVendorWithNumber("1618048");
+	}
+	
+		@Test
+		public void testC939934()
+		{
+			//Initialization
+			LoginPage loginPage = new LoginPage(TestTemplate.threadLocalWebDriver.get(), TestTemplate.testReport);
+			JdeVendorSearchPage jdeVendorSearchPage = new JdeVendorSearchPage(TestTemplate.threadLocalWebDriver.get(), TestTemplate.testReport);
+			
+			//Logging in to application
+			loginPage.jdeVendorSearchLogin();
+					
+			//search with Vendor Number and validate record
+			jdeVendorSearchPage.searchForAVendorWithAllFields("Verizon", "TX", "75266-0720", "1618048");
+		}
 }
