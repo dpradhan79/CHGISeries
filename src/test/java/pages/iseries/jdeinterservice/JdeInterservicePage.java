b3def11c0c8f19fb666bfff2dfc88ff35eaf1adb
Anil <anil.talasila@cigniti.com>
2018-08-24 17:46:15 +0530

    Created new classes for QAEvals and JDE interservice
    Added 2 new test cases in QAEvals

---
+++ src/test/java/pages/iseries/jdeinterservice/JdeInterservicePage.java
@@ -0,0 +1,14 @@
+package pages.iseries.jdeinterservice;
+
+import org.openqa.selenium.WebDriver;
+
+import com.testreport.IReporter;
+
+import dweb.aut.pages.templates.PageTemplate;
+
+public class JdeInterservicePage extends PageTemplate {
+
+	public JdeInterservicePage(WebDriver webDriver, IReporter testReport) {
+		super(webDriver, testReport);
+	}
+}
---
+++ src/test/java/test/iseries/jdeinterservice/TestJDEinterservice.java
@@ -0,0 +1,23 @@
+package test.iseries.jdeinterservice;
+
+import org.testng.annotations.Test;
+
+import dweb.test.templates.TestTemplate;
+import dweb.test.templates.TestTemplateMethodLevelInit;
+import pages.iseries.jdeinterservice.JdeInterservicePage;
+import pages.iseries.loginpage.LoginPage;
+
+public class TestJDEinterservice extends TestTemplateMethodLevelInit{
+
+	@Test
+	public void testC939958()
+	{
+		//Initialization
+		LoginPage loginPage = new LoginPage(TestTemplate.threadLocalWebDriver.get(), TestTemplate.testReport);
+		JdeInterservicePage jdeInterServicePage = new JdeInterservicePage(TestTemplate.threadLocalWebDriver.get(), TestTemplate.testReport);
+		
+		//Logging in to application
+		loginPage.jdeVendorSearchLogin();
+
+	}
+}
