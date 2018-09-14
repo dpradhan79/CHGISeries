bb551ed65874cdcdab5e926afa3d9a277d82f18d
Debasish Pradhan <debasish.pradhan@gmail.com>
2018-09-14 05:02:09 -0600

    Added Tests to RejectedPractitioners

--- src/test/java/pages/iseries/rejectedpractitioners/RejectedPractitionersPage.java
+++ src/test/java/pages/iseries/rejectedpractitioners/RejectedPractitionersPage.java
@@ -1,14 +1,32 @@
 package pages.iseries.rejectedpractitioners;
 
+import java.util.Map;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.ITestContext;
 import org.testng.asserts.SoftAssert;
+
 import com.testreport.IReporter;
+
 import dweb.aut.pages.templates.PageTemplate;
 
 public class RejectedPractitionersPage extends PageTemplate {
 
-	
-	private SoftAssert softAssert = null;
+	private By userName									= By.xpath("//input[contains(@name,'j_username')]");
+	private By password 								= By.xpath("//input[contains(@name,'password')]");
+	private By loginButton 								= By.xpath("//input[contains(@value,'Login')]");
+	private By searchCOMPHEALTHButton					= By.xpath("//input[@value='SEARCH COMPHEALTH']");
+	private By searchRejectedPractitionersLink		 	= By.xpath("//a[text()='Search Rejected Practitioners']");
+	private By rejectedPractitionersHomePage		 	= By.xpath("//form[contains(@action,'RejectedPractitionerSearch')]");
+	private By byLastName 								= By.xpath("//input[@name='lastName']");
+    private By byFirstName								= By.xpath("//input[@name='firstName']");
+    private By byDateOfBirth 							= By.xpath("//input[@name='dob']");
+    private By bySocialSecurity							= By.xpath("//input[@name='ssn']");
+    private By byResetbtn 								= By.xpath("//input[@value='RESET']");
+
+    private SoftAssert softAssert = null;
 	public RejectedPractitionersPage(WebDriver webDriver, IReporter testReport) {
 		super(webDriver, testReport);
 		this.softAssert = null;
@@ -18,6 +36,219 @@ public class RejectedPractitionersPage extends PageTemplate {
 		super(webDriver, testReport);
 		this.softAssert = softAssert;
 	}
+	
+	/**
+	 * Login to rejectedPractitioners Login application
+	 */
+	public void rejectedPractitionersLogin(ITestContext testContext)
+	{
+		//Wait until user name displayed then enter user name
+		this.waitUntilElementIsVisible(userName);
+		this.sendKeys(userName, testContext.getCurrentXmlTest().getParameter("userName"));
+		//Enter password
+		this.sendKeys(password, testContext.getCurrentXmlTest().getParameter("password"));
+		
+		//Click on login button
+		this.click(loginButton);
+	}
+	
+	/**
+	 * Click on Search COMPHEALTH button
+	 */
+	private void clickOnSearchCompHealthButtonk() 
+	{
+		this.waitUntilElementIsClickable(searchCOMPHEALTHButton);
+		this.click(searchCOMPHEALTHButton);
+	}
+
+	/**
+	 * Click on Search Rejected Practitioners Link
+	 */
+	private void clickOnRejectedPractitionersLink() 
+	{
+		this.waitUntilElementIsClickable(searchRejectedPractitionersLink);
+		this.click(searchRejectedPractitionersLink);
+	}
+
+	
+	/**
+	 * Click on Search Rejected Practitioners Link
+	 */
+	private void VerifyRejectedPractitionersText() 
+	{
+		this.waitUntilElementIsVisible(rejectedPractitionersHomePage);
+		
+		WebElement element = this.wd.findElement(rejectedPractitionersHomePage);
+		
+		if (element.isDisplayed()) {
+
+            this.testReport.logSuccess("User Successfully back to to search page onclicking on Search Rejected Practioners link ");
+
+     } else {
+
+            this.testReport.logFailure("User Failed back to search page onclicking on Search Rejected Practioners link " + rejectedPractitionersHomePage,
+
+                         this.getScreenShotName());
+
+     }
+		
+	}
+
+	
+   public void verifyUserBackToSearchPage(ITestContext testContext) {
+		// TODO Auto-generated method stub
+		
+		// click on Search COMPHEALTH button
+		
+		clickOnSearchCompHealthButtonk();
+		
+		// click on Search Rejected Practitioners Link
+		
+		clickOnRejectedPractitionersLink();
+		
+		// Verify that clicking on Search Rejected Practioners link takes user to search page
+		
+		VerifyRejectedPractitionersText();
+	}
+	
+   /**
+
+    * @param map
+
+    *            contains all the data from Excel
+
+    *
+
+     *            Fill all the fields on Rejected Practitioner search page
+
+    */
+
+    public void fillAllFieldsInRejectedPractioners(Map<String, String> map) {
+
+
+
+           // Fill all the fields
+
+           for (Map.Entry<String, String> entrySet : map.entrySet()) {
+
+                  String searchCriteria = entrySet.getKey();
+
+                  String searchValue = entrySet.getValue();
+
+
+
+                  switch (searchCriteria) {
+
+                  case "LastName":
+
+                        this.sendKeys(byLastName, searchValue);
+
+                        break;
+
+                  case "FirstName":
+
+                        this.sendKeys(byFirstName, searchValue);
+
+                        break;
+
+                  case "DateOfBirth":
+
+                        this.sendKeys(byDateOfBirth, searchValue);
+
+                        break;
+
+                  case "SocialSecurity":
+
+                        this.sendKeys(bySocialSecurity, searchValue);
+
+                        break;
+
+
+
+                  }
+
+
+
+           }
+
+
+
+    }
+
+
+
+    /**
+
+    * Click on Reset and Validate fields are Reseted
+
+    */
+
+
+
+    public void resetFieldsInRejectedPractioners() {
+
+
+
+           // Click on Reset button
+
+           this.click(byResetbtn);
+
+
+
+           // Validating fields are Reset
+
+           this.isEmptyField(byLastName);
+
+           this.isEmptyField(byFirstName);
+
+           this.isEmptyField(byDateOfBirth);
+
+           this.isEmptyField(bySocialSecurity);
+
+
+
+    }
+
+
+
+    /**
+
+    * * @param By locator of WebElement Verify the field is Empty
+
+    */
+
+
+
+    private void isEmptyField(By by) {
+
+
+
+           WebElement e = this.wd.findElement(by);
+
+
+
+           this.softAssert.assertTrue(e.getText().isEmpty());
+
+
+
+           if (e.getText().isEmpty()) {
+
+                  this.testReport.logSuccess("Rejected Practitioner page",
+
+                               "Field with Locator :" + by + " is Successfully Reseted");
+
+           } else {
+
+                  this.testReport.logFailure("Rejected Practitioner page", "Unable To Reset field with locator:" + by,
+
+                               this.getScreenShotName());
+
+           }
+
+    }
+
+
+	
 
 }
 	
--- src/test/java/test/iseries/rejectedpractitioners/TestRejectedPractitioners.java
+++ src/test/java/test/iseries/rejectedpractitioners/TestRejectedPractitioners.java
@@ -1,8 +1,111 @@
 package test.iseries.rejectedpractitioners;
 
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+import org.testng.ITestContext;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.config.IConstants;
+import com.excel.Xls_Reader;
+import com.google.common.io.Resources;
+import com.utilities.TestUtil;
+
+import pages.iseries.rejectedpractitioners.RejectedPractitionersPage;
+import dweb.test.templates.TestTemplate;
 import dweb.test.templates.TestTemplateMethodLevelInit;
 
 public class TestRejectedPractitioners extends TestTemplateMethodLevelInit {
+	
 
+    @DataProvider(name = "getRejectedPractitionersDataFromExcel", parallel = false)
+
+    protected Object[][] getGMEdataFromExcel() throws URISyntaxException {
+
+           URL urlFilePath = Resources.getResource(String.format("%s/%s", IConstants.TEST_DATA_LOCATION, IConstants.TEST_DATA_EXCEL_FILE));
+
+           String filePath = Paths.get(urlFilePath.toURI()).toFile().getAbsolutePath();
+
+           Xls_Reader xlsReader = new Xls_Reader(filePath);
+
+           Object[][] objMetrics = TestUtil.getData("RejectedPractitionersData", xlsReader, "RejectedPractitioners");
+
+           return objMetrics;
+
+    }
+	
+    // description = "Verify that clicking on Search Rejected Practioners link takes user to search page"
+
+	@Test(dataProvider = "getRejectedPractitionersDataFromExcel")
+	public void testC863584(Hashtable<String, String> data, ITestContext testContext)
+	{
+		//Initialization
+		RejectedPractitionersPage rejectedPractitionersPage = new RejectedPractitionersPage(TestTemplate.threadLocalWebDriver.get(), TestTemplate.testReport, this.softAssert);
+		
+		
+	    // Logging in to Rejected Practitioners
+
+        rejectedPractitionersPage.rejectedPractitionersLogin(testContext);
+        
+        
+        // Enter Data into first Name field
+        
+        Map<String, String> mapidsSerachCriteria = new HashMap<String, String>();
+		
+		mapidsSerachCriteria.put("FirstName", data.get("FirstName"));
+		
+		rejectedPractitionersPage.fillAllFieldsInRejectedPractioners(mapidsSerachCriteria);
+		
+		//Verify that clicking on Search Rejected Practioners link takes user to search page
+		
+		rejectedPractitionersPage.verifyUserBackToSearchPage(testContext);
+		
+
+		this.softAssert.assertAll();
+	}
+	
+	  @Test(dataProvider = "getRejectedPractitionersDataFromExcel", description = "Reset Rejected Practitioners fields")
+
+      public void testC863583(ITestContext testContext, Hashtable<String, String> data) {
+
+             // Data
+
+             Map<String, String> map = new HashMap<String, String>();
+
+             map.put("LastName", data.get("LastName"));
+
+             map.put("FirstName", data.get("FirstName"));
+
+             map.put("DateOfBirth", data.get("DateOfBirth"));
+
+             map.put("SocialSecurity", data.get("SocialSecurity"));
+
+             // Initialization
+
+             RejectedPractitionersPage rejectedPractitionersPage = new RejectedPractitionersPage(
+
+                          TestTemplate.threadLocalWebDriver.get(), TestTemplate.testReport, this.softAssert);
+
+             // Logging in to Rejected Practitioners
+
+             rejectedPractitionersPage.rejectedPractitionersLogin(testContext);
+
+             // Fill all the fields in Rejected Practitioners Search
+
+             rejectedPractitionersPage.fillAllFieldsInRejectedPractioners(map);
+
+             // Verify fields are Reseted
+
+             rejectedPractitionersPage.resetFieldsInRejectedPractioners();
+
+             this.softAssert.assertAll();
+
+      }
+	
 	
 }
