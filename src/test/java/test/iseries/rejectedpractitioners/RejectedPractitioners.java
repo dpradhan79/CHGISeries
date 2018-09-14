a8c18a651c7a59d135e4f19474c8ae1bc52da3b6
E002490 <E002490@HW1262.cignitiglobal.com>
2018-09-14 13:32:41 +0530

    changed file

--- src/test/java/test/iseries/rejectedpractitioners/RejectedPractitioners.java
+++
@@ -1,9 +0,0 @@
-package test.iseries.rejectedpractitioners;
-
-import dweb.test.templates.TestTemplateMethodLevelInit;
-
-public class RejectedPractitioners extends TestTemplateMethodLevelInit {
-
-	
-	
-}
